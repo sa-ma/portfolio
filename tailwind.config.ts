20fbcb659b6639e9a5ce9c8e48de62c3f470cde3
Samaila Bala <samailabalap@gmail.com>
2023-12-17 16:37:55 +0100

    initialize project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,20 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: ['class'],
+    content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}', './app/**/*.{ts,tsx}', './src/**/*.{ts,tsx}'],
+    theme: {
+        container: {
+            center: true,
+            padding: '2rem',
+            screens: {
+                '2xl': '1400px',
+            },
+        },
+        extend: {
+            fontFamily: {
+                sans: ['var(--font-inter)'],
+            },
+        },
     },
-  },
-  plugins: [],
-}
-export default config
+    plugins: [require('tailwindcss-animate')],
+};
