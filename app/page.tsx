6e45a6ce1d766cee6bf08d4a29cbb9cd1d2a5c8b
Samaila Bala <samailabalap@gmail.com>
2023-12-18 03:19:13 +0100

    make main nav responsive

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,12 @@ import Link from 'next/link';
 export default function Home() {
     return (
         <main className='w-screen h-screen bg-primary'>
-            <div className='absolute bottom-0 left-0 p-4 w-full flex justify-between items-center'>
+            <div className='absolute bottom-0 left-0 p-4 w-full flex flex-col md:flex-row justify-between items-start md:items-center'>
                 <Link href='mailto:samailabalap@gmail.com' className='text-typography'>
                     samailabalap@gmail.com
                 </Link>
 
-                <div className='flex items-center gap-2'>
+                <div className='flex flex-col md:flex-row items-start md:items-center gap-0 md:gap-2'>
                     <Link className='text-typography' href='/blog'>
                         Writings
                     </Link>
